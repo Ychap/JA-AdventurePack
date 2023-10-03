2a91044a5f7dd1192c00ae5d2da40eb74c892d2e
Ychap <asdfsa85@web.de>
2023-10-03 15:36:46 +0200

    Remove unnecessary response

--- JA#BGT_AdvPack/Shop_of_Silence/ja#sth02.d
+++ JA#BGT_AdvPack/Shop_of_Silence/ja#sth02.d
@@ -9,20 +9,20 @@ END
 
 IF ~~ THEN BEGIN JA#STH02_2
 SAY @4
-IF ~~ THEN REPLY @5 DO ~SetGlobal("JA#KNOW_MASKTEMPLE","GLOBAL",1)EscapeArea()~ EXIT
+IF ~~ THEN DO ~SetGlobal("JA#KNOW_MASKTEMPLE","GLOBAL",1) EscapeArea()~ EXIT
 END
 
-IF ~ReputationGT(LastTalkedToBy(Myself),15)RandomNum(3,1)~ THEN BEGIN JA#STH02_3
+IF ~ReputationGT(LastTalkedToBy(Myself),15) RandomNum(3,1)~ THEN BEGIN JA#STH02_3
 SAY @6
 IF ~~ THEN EXIT
 END
 
-IF ~ReputationGT(LastTalkedToBy(Myself),15)RandomNum(3,2)~ THEN BEGIN JA#STH02_4
+IF ~ReputationGT(LastTalkedToBy(Myself),15) RandomNum(3,2)~ THEN BEGIN JA#STH02_4
 SAY @7
 IF ~~ THEN EXIT
 END
 
-IF ~ReputationGT(LastTalkedToBy(Myself),15)RandomNum(3,3)~ THEN BEGIN JA#STH02_5
+IF ~ReputationGT(LastTalkedToBy(Myself),15) RandomNum(3,3)~ THEN BEGIN JA#STH02_5
 SAY @8
 IF ~~ THEN EXIT
 END
@@ -31,8 +31,3 @@ IF ~~ THEN BEGIN JA#STH02_6
 SAY @9
 IF ~~ THEN DO ~EscapeArea()~ EXIT
 END
-
-
-
-
-
