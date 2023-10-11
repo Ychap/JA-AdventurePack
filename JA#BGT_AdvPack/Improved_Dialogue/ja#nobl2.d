4ce71a6dd4093800717093ad26cfe1e583ca1000
Ychap <asdfsa85@web.de>
2023-10-11 19:06:06 +0200

    More flexible replacement in ja#nobl2.d

--- JA#BGT_AdvPack/Improved_Dialogue/ja#nobl2.d
+++ JA#BGT_AdvPack/Improved_Dialogue/ja#nobl2.d
@@ -1,18 +1,17 @@
-REPLACE_STATE_TRIGGER NOBL2 0 ~NumTimesTalkedTo(0)~
+REPLACE_STATE_TRIGGER NOBL2 0
+~NumTimesTalkedTo(0)~
+IF ~True()~
 
-ALTER_TRANS NOBL2 // file name
-BEGIN 0 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "ACTION" ~NoAction()~
-END
+REPLACE_ACTION_TEXT NOBL2
+~EscapeArea\(Destroy\)?([0-9]*)~
+~NoAction()~
 
 
 APPEND NOBL2
 
-IF ~NumTimesTalkedToGT(0)~ THEN BEGIN JA#NOBL2_1
+IF ~True()~ THEN BEGIN JA#NOBL2_1
 SAY @0
 IF ~~ THEN EXIT
 END
 
-END
+END // APPEND NOBL2
