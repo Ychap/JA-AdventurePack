8039eea33832c1c805986c6fc02bd9894b2786bc
Ychap <asdfsa85@web.de>
2023-10-13 19:54:33 +0200

    More flexible replacement in JA#Vai.d

--- JA#BGT_AdvPack/Improved_Dialogue/JA#Vai.d
+++ JA#BGT_AdvPack/Improved_Dialogue/JA#Vai.d
@@ -1,9 +1,3 @@
-//Officer Vai
-
-ALTER_TRANS VAI // file name
-BEGIN 8 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "ACTION" ~ActionOverride("FLAMVAI",Enemy())
-Enemy()~
-END
+REPLACE_ACTION_TEXT VAI
+~Enemy()~
+~Enemy() ActionOverride("FLAMVAI",Enemy())~
