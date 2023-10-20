bb0544ecd3badd6a19ca63a3dbbbda89f5e91203
Ychap <asdfsa85@web.de>
2023-10-20 17:00:05 +0200

    Fix Phandalyn component

--- JA#BGT_AdvPack/Phandalyn/Bellamy.d
+++ JA#BGT_AdvPack/Phandalyn/Bellamy.d
@@ -1,15 +1,4 @@
-//Bellamy
-
-ALTER_TRANS BELLAM // file name
-BEGIN 3 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flagsdu
-  "ACTION" ~SetGlobal("JA#PHANDA","GLOBAL",1)~
-END
-
-ALTER_TRANS BELLAM // file name
-BEGIN 4 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "ACTION" ~SetGlobal("JA#PHANDA","GLOBAL",1)~
-END
+ADD_TRANS_ACTION BELLAM
+BEGIN 0 1 END
+BEGIN END
+~SetGlobal("JA#PHANDA","GLOBAL",1)~
--- JA#BGT_AdvPack/Phandalyn/Phandaly.d
+++ JA#BGT_AdvPack/Phandalyn/Phandaly.d
@@ -1,42 +1,39 @@
-//Paladin Phandalyn
-
-ALTER_TRANS PHANDA // file name
-BEGIN 1 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "ACTION" ~NoAction()~
-END
-
 REPLACE_STATE_TRIGGER PHANDA 2 ~NumTimesTalkedToGT(0)
 InParty([0.0.0.0.0.0.MASK_EVIL])
 See([PC.0.0.0.0.0.MASK_EVIL])~
 
-ALTER_TRANS PHANDA // file name
-BEGIN 2 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "ACTION" ~Attack([0.0.0.0.0.0.MASK_EVIL])~
-  "JOURNAL" ~@7~
+EXTEND_BOTTOM PHANDA 2
+COPY_TRANS PHANDA 0
 END
+
 
 REPLACE_STATE_TRIGGER PHANDA 3 ~NumTimesTalkedToGT(0)~
 
-ALTER_TRANS PHANDA // file name
-BEGIN 3 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "REPLY" ~@11~
+ALTER_TRANS PHANDA
+BEGIN 3 END
+BEGIN 0 END
+BEGIN
+  "REPLY" ~@1~
 END
 
 EXTEND_BOTTOM PHANDA 3
-IF ~~ THEN REPLY @5 GOTO JA#PHANDA_1
+IF ~~ THEN REPLY @2 GOTO JA#PHANDA_1
 END
 
 APPEND PHANDA
 
 IF ~~ THEN BEGIN JA#PHANDA_1
-SAY @6
-IF ~~ THEN DO ~Enemy()
-Attack([0.0.0.0.0.0.MASK_EVIL])~ JOURNAL @7 EXIT
+SAY @3
+COPY_TRANS PHANDA 0
 END
-END
+
+END // APPEND PHANDA
+
+
+REPLACE_ACTION_TEXT PHANDA
+~Attack(\[0\.0\.0\.0\.0\.0\.MASK_EVIL\])~
+~Attack([PC.0.0.0.0.0.MASK_EVIL])~
+
+REPLACE_ACTION_TEXT PHANDA
+~EscapeArea()~
+~NoAction()~
