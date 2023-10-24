1f1c694e29ff5a934ecff2f5c0341c43611e48e9
Ychap <asdfsa85@web.de>
2023-10-24 18:30:32 +0200

    Move changes of DUNKIN.DLG in own file

---
+++ JA#BGT_AdvPack/Marl/ja#dunkin.d
@@ -0,0 +1,11 @@
+REPLACE_STATE_TRIGGER DUNKIN 5 ~Global("JA#DUNKIN","%Beregost_FeldepostsInn_L1%",1)~
+
+ADD_TRANS_ACTION DUNKIN
+BEGIN 5 END
+BEGIN END
+~SetGlobal("JA#DUNKIN","%Beregost_FeldepostsInn_L1%",2)~
+
+ADD_TRANS_ACTION DUNKIN
+BEGIN 7 END
+BEGIN END
+~ClearAllActions()~
--- JA#BGT_AdvPack/Marl/ja#keldda.d
+++ JA#BGT_AdvPack/Marl/ja#keldda.d
@@ -35,23 +35,3 @@ BEGIN END
 BEGIN
   "ACTION" ~SetGlobal("B!GavinBassilusQuest","GLOBAL",5) %state1_response0_action%~
 END
-
-
-//Dunkin
-
-ALTER_TRANS DUNKIN
-BEGIN 7 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "ACTION" ~ClearAllActions()~
-END
-
-//misc.d
-REPLACE_STATE_TRIGGER DUNKIN 5 ~Global("JA#DUNKIN","%Beregost_FeldepostsInn_L1%",1)~
-
-ALTER_TRANS DUNKIN // file name
-BEGIN 5 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "ACTION" ~SetGlobal("JA#DUNKIN","%Beregost_FeldepostsInn_L1%",2)~
-END
