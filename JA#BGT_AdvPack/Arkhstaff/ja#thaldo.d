be1106df75e374682a3c570f6b887b086ad1d604
Ychap <asdfsa85@web.de>
2023-10-30 10:16:29 +0100

    Use COPY_TRANS in ja#thaldo.d

--- JA#BGT_AdvPack/Arkhstaff/ja#thaldo.d
+++ JA#BGT_AdvPack/Arkhstaff/ja#thaldo.d
@@ -1,49 +1,16 @@
-ALTER_TRANS THALDO // file name
-BEGIN 2 END // state number (can be more than one)
-BEGIN 1 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "ACTION" ~SetGlobal("JA#SAVETHALDO","GLOBAL",1)EscapeAreaDestroy(90)~
+REPLACE_ACTION_TEXT THALDO
+~EscapeArea\(Destroy\)?([0-9]*)~
+~SetGlobal("JA#SAVETHALDO","GLOBAL",1) \0~
+
+REPLACE_ACTION_TEXT THALDO
+~[%WNL%%MNL%%LNL%%TAB% ]Enemy()~
+~EraseJournalEntry(@1010)\0~
+
+
+ADD_STATE_TRIGGER THALDO 6 ~True()~
+
+ADD_TRANS_TRIGGER THALDO 6 ~False()~ DO 0
+
+EXTEND_TOP THALDO 6 #1
+COPY_TRANS THALDO 0
 END
-
-ALTER_TRANS THALDO // file name
-BEGIN 3 END // state number (can be more than one)
-BEGIN 1 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "ACTION" ~SetGlobal("JA#SAVETHALDO","GLOBAL",1)EscapeAreaDestroy(90)~
-END
-
-ALTER_TRANS THALDO // file name
-BEGIN 4 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "ACTION" ~SetGlobal("JA#SAVETHALDO","GLOBAL",1)EscapeAreaDestroy(90)~
-END
-
-ALTER_TRANS THALDO // file name
-BEGIN 4 END // state number (can be more than one)
-BEGIN 1 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "ACTION" ~EraseJournalEntry(@1010)~
-END
-
-ADD_STATE_TRIGGER THALDO 6 ~NumTimesTalkedToGT(0)~
-
-ALTER_TRANS THALDO // file name
-BEGIN 6 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "REPLY" ~@1~
-  "EPILOGUE" ~GOTO 1~
-END
-
-EXTEND_BOTTOM THALDO 6
-IF ~~ THEN REPLY @0 EXIT
-END
-
-
-
-
-
-
-
-
