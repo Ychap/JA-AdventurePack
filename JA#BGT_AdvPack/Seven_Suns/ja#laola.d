d09b66717eb81fb2b36e977beb57770de8e36e94
Ychap <asdfsa85@web.de>
2023-11-07 20:03:44 +0100

    Shorter and more flexible ja#laola.d

--- JA#BGT_AdvPack/Seven_Suns/ja#laola.d
+++ JA#BGT_AdvPack/Seven_Suns/ja#laola.d
@@ -1,28 +1,18 @@
-//Laola Axehand
 
-ALTER_TRANS LAOLA // file name
-BEGIN 0 END // state number (can be more than one)
-BEGIN 1 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "TRIGGER" ~Global("ScarMission","GLOBAL",0)~
-END
+ADD_TRANS_TRIGGER LAOLA 0
+~Global("ScarMission","GLOBAL",0)~ DO 1 2
 
-ALTER_TRANS LAOLA // file name
-BEGIN 0 END // state number (can be more than one)
-BEGIN 2 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "TRIGGER" ~Global("ScarMission","GLOBAL",0)~
-END
 
 EXTEND_BOTTOM LAOLA 0
 IF ~Global("ScarMission","GLOBAL",1)~ THEN REPLY @0 GOTO JA#LAOLA_1
 END
 
+
 APPEND LAOLA
 
 IF ~~ THEN BEGIN JA#LAOLA_1
-SAY @1
-IF ~~ THEN DO ~EscapeAreaDestroy(90)~ EXIT
+  SAY @1
+  COPY_TRANS LAOLA 3
 END
 
-END
+END // APPEND LAOLA
