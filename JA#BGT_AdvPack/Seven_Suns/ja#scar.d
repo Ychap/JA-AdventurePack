79f196b5d235a28a5cebc1881d6d531d637455d0
Ychap <asdfsa85@web.de>
2023-11-07 19:41:19 +0100

    Reduce trigger additions in SCAR.DLG to a minimum
    Extra dialog is only suitable in BGT
    EE has extra dialog option for Jhasso's death

--- JA#BGT_AdvPack/Seven_Suns/ja#scar.d
+++ JA#BGT_AdvPack/Seven_Suns/ja#scar.d
@@ -1,56 +1,10 @@
 
-ALTER_TRANS SCAR // file name
-BEGIN 14 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "TRIGGER" ~Global("JA#DOPPSE_TALK","GLOBAL",0)~
-END
+ADD_TRANS_TRIGGER SCAR 8
+~Global("JA#DOPPSE_TALK","GLOBAL",0)~ DO 0 1
 
-ALTER_TRANS SCAR // file name
-BEGIN 14 END // state number (can be more than one)
-BEGIN 1 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "TRIGGER" ~NumDeadGT("SSDOPPLE",4)
-Global("JA#DOPPSE_TALK","GLOBAL",1)
-!Dead("Jhasso")~
-END
+ADD_TRANS_TRIGGER SCAR 8
+~Global("JA#DOPPSE_TALK","GLOBAL",1)~ DO 2
 
-ALTER_TRANS SCAR // file name
-BEGIN 14 END // state number (can be more than one)
-BEGIN 2 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "TRIGGER" ~NumDeadGT("SSDOPPLE",4)
-Global("Doppleganger","GLOBAL",1)
-Dead("Jhasso")~
-END
 
-ALTER_TRANS SCAR // file name
-BEGIN 8 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "TRIGGER" ~Global("JA#DOPPSE_TALK","GLOBAL",0)~
-END
-
-ALTER_TRANS SCAR // file name
-BEGIN 8 END // state number (can be more than one)
-BEGIN 1 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "TRIGGER" ~GlobalLT("AldethDopple","GLOBAL",16)
-NumDeadLT("SSDOPPLE",5)
-Global("CheckedSevenSuns","GLOBAL",2)
-Global("JA#DOPPSE_TALK","GLOBAL",0)~
-END
-
-ALTER_TRANS SCAR // file name
-BEGIN 8 END // state number (can be more than one)
-BEGIN 2 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "TRIGGER" ~NumDeadGT("SSDOPPLE",4)
-Global("JA#DOPPSE_TALK","GLOBAL",1)
-!Dead("jhasso")~
-END
-
-EXTEND_TOP SCAR 8 #3
-+ ~Global("Doppleganger","GLOBAL",1)
-Dead("jhasso")~ + @0 DO ~GiveGoldForce(4000)AddexperienceParty(4000)~ GOTO 17
-END
+ADD_TRANS_TRIGGER SCAR 14
+~Global("JA#DOPPSE_TALK","GLOBAL",0)~ DO 0
---
+++ JA#BGT_AdvPack/Seven_Suns/ja#scar_bgt.d
@@ -0,0 +1,27 @@
+
+REPLACE_TRIGGER_TEXT SCAR
+~GlobalGT("KilledDopple","GLOBAL",\([0-9]+\))~
+~NumDeadGT("SSDOPPLE",\1)~
+
+REPLACE_TRIGGER_TEXT SCAR
+~Global("KilledDopple","GLOBAL",\([0-9]+\)~
+~OR(2) NumDead("SSDOPPLE",\1) NumDeadGT("SSDOPPLE",\1)~
+
+
+ADD_TRANS_TRIGGER SCAR 8
+~NumDeadLT("SSDOPPLE",5)~ DO 1
+
+ADD_TRANS_TRIGGER SCAR 8
+~!Dead("Jhasso")~ DO 2
+
+
+EXTEND_TOP SCAR 8 #3
+IF ~NumDeadGT("SSDOPPLE",4) Dead("Jhasso")~ THEN REPLY @0 GOTO 17
+END
+
+
+ADD_TRANS_ACTION SCAR
+BEGIN 17 END
+BEGIN 0 END
+~AddexperienceParty(4000)~
+UNLESS ~AddexperienceParty~
