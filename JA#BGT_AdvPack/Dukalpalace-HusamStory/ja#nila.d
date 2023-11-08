3bb306d9a10f2681386fa64309127f71fc9dd882
Ychap <asdfsa85@web.de>
2023-11-08 21:51:47 +0100

    Make use of WEIGHT in ja#nila.d

--- JA#BGT_AdvPack/Dukalpalace-HusamStory/ja#nila.d
+++ JA#BGT_AdvPack/Dukalpalace-HusamStory/ja#nila.d
@@ -1,71 +1,46 @@
-ADD_STATE_TRIGGER NILA 0 ~
-Global("JA#NILA","%NBaldursGate_DucalPalace_L2%",0)
-Global("JA#CONTAINER2","%NBaldursGate_DucalPalace_L2%",0)~
-ADD_STATE_TRIGGER NILA 1 ~
-Global("JA#NILA","%NBaldursGate_DucalPalace_L2%",0)
-Global("JA#CONTAINER2","%NBaldursGate_DucalPalace_L2%",0)~
+ADD_STATE_TRIGGER NILA 0
+~Global("JA#NILA","%NBaldursGate_DucalPalace_L2%",0) Global("JA#CONTAINER2","%NBaldursGate_DucalPalace_L2%",0)~
+ADD_STATE_TRIGGER NILA 1
+~Global("JA#NILA","%NBaldursGate_DucalPalace_L2%",0) Global("JA#CONTAINER2","%NBaldursGate_DucalPalace_L2%",0)~
 
-REPLACE_STATE_TRIGGER NILA 2 ~
-Global("JA#CONTAINER2","%NBaldursGate_DucalPalace_L2%",1)~
 
-REPLACE_STATE_TRIGGER NILA 7~
-Global("JA#NILA","%NBaldursGate_DucalPalace_L2%",3)
-Global("JA#CONTAINER2","%NBaldursGate_DucalPalace_L2%",2)~
+REPLACE_STATE_TRIGGER NILA 7
+~Global("JA#NILA","%NBaldursGate_DucalPalace_L2%",3) Global("JA#CONTAINER2","%NBaldursGate_DucalPalace_L2%",2)~
 
-REPLACE_STATE_TRIGGER NILA 8 ~
-NumTimesTalkedToGT(0)
-!Global("JA#CONTAINER2","%NBaldursGate_DucalPalace_L2%",1)
-!Global("JA#NILA","%NBaldursGate_DucalPalace_L2%",1)
-!Global("JA#NILA","%NBaldursGate_DucalPalace_L2%",2)~
+REPLACE_STATE_TRIGGER NILA 8
+~True()~
 
-ALTER_TRANS NILA // file name
-BEGIN 0 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "ACTION" ~SetGlobal("JA#NILA_WHO","LOCALS",1)~
-  "REPLY" ~@8~
-  "EPILOGUE" ~GOTO JA#NILA_3~
-END
 
-ALTER_TRANS NILA // file name
-BEGIN 1 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "ACTION" ~SetGlobal("JA#NILA_WHO","LOCALS",1)~
-  "REPLY" ~@8~
-  "EPILOGUE" ~GOTO JA#NILA_3~
-END
+SET_WEIGHT NILA 2 #-1
 
-ALTER_TRANS NILA // file name
-BEGIN 3 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "ACTION" ~SetGlobal("JA#CONTAINER2","%NBaldursGate_DucalPalace_L2%",2)
-MoveToPoint([522.640])~
-END
+REPLACE_STATE_TRIGGER NILA 2
+~Global("JA#CONTAINER2","%NBaldursGate_DucalPalace_L2%",1)~
 
+ADD_TRANS_ACTION NILA
+BEGIN 3 END
+BEGIN END
+~SetGlobal("JA#CONTAINER2","%NBaldursGate_DucalPalace_L2%",2) MoveToPoint([522.640])~
+
+
+ADD_TRANS_TRIGGER NILA 0 ~False()~
+ADD_TRANS_TRIGGER NILA 1 ~False()~
 
 EXTEND_BOTTOM NILA 0
+IF ~~ THEN REPLY @8 DO ~SetGlobal("JA#NILA_WHO","LOCALS",1)~ GOTO JA#NILA_3
 IF ~~ THEN REPLY @0 DO ~SetGlobal("JA#SARE","LOCALS",1)~ GOTO JA#NILA_4
-END
-
-EXTEND_BOTTOM NILA 0
 IF ~~ THEN REPLY @1 GOTO JA#NILA_5
 END
 
 EXTEND_BOTTOM NILA 1
+IF ~~ THEN REPLY @8 DO ~SetGlobal("JA#NILA_WHO","LOCALS",1)~ GOTO JA#NILA_3
 IF ~~ THEN REPLY @0 DO ~SetGlobal("JA#SARE","LOCALS",1)~ GOTO JA#NILA_4
-END
-
-EXTEND_BOTTOM NILA 1
 IF ~~ THEN REPLY @1 GOTO JA#NILA_5
 END
 
 
 APPEND NILA
 
-IF ~
-Global("JA#NILA","%NBaldursGate_DucalPalace_L2%",1)~ THEN BEGIN JA#NILA_1
+IF WEIGHT #-1 ~Global("JA#NILA","%NBaldursGate_DucalPalace_L2%",1)~ THEN BEGIN JA#NILA_1
 SAY @2
 IF ~~ THEN DO ~SetGlobal("JA#NILA","%NBaldursGate_DucalPalace_L2%",2)
 DialogInterrupt(FALSE)
@@ -79,11 +54,11 @@ DialogInterrupt(TRUE)
 Dialogue([PC])~ EXIT
 END
 
-IF ~
-Global("JA#NILA","%NBaldursGate_DucalPalace_L2%",2)~ THEN BEGIN JA#NILA_2
+IF WEIGHT #-1 ~Global("JA#NILA","%NBaldursGate_DucalPalace_L2%",2)~ THEN BEGIN JA#NILA_2
 SAY @3
-IF ~~ THEN DO ~SetGlobal("JA#NILA","%NBaldursGate_DucalPalace_L2%",3)MoveToPoint([522.640])~ EXIT
+IF ~~ THEN DO ~SetGlobal("JA#NILA","%NBaldursGate_DucalPalace_L2%",3) MoveToPoint([522.640])~ EXIT
 END
+
 
 IF ~~ THEN BEGIN JA#NILA_3
 SAY @4
@@ -99,7 +74,7 @@ END
 
 IF ~~ THEN BEGIN JA#NILA_5
 SAY @7
-IF ~~ THEN EXIT
+COPY_TRANS NILA 0
 END
 
-END
+END // APPEND NILA
