70b4039005af7c7bf0f646f234d8fc358e8b3ae3
Ychap <asdfsa85@web.de>
2024-03-02 23:26:03 +0100

    Fix ja#aldeth_reward.d

--- JA#BGT_AdvPack/Seniyad/ja#aldeth_reward.d
+++ JA#BGT_AdvPack/Seniyad/ja#aldeth_reward.d
@@ -3,11 +3,11 @@ REPLACE_ACTION_TEXT ALDETH
 ~GiveItem("JA#ALDET"~
 
 
-APPEND Aldeth
+APPEND ALDETH
 
-IF ~GlobalLT("Chapter","GLOBAL",%tutu_chapter_5%) Global("SeniyadXP","GLOBAL",1)~ THEN BEGIN
+IF ~GlobalLT("Chapter","GLOBAL",%tutu_chapter_5%) Global("SeniyadXP","GLOBAL",1)~ THEN BEGIN JA#ALDETH_X0
 SAY @18 = @19
 IF ~~ THEN DO ~SetGlobal("AldethMove","GLOBAL",1) GiveItem("JA#ALDET",LastTalkedToBy) Shout(5) AddexperienceParty(600) EraseJournalEntry(%strref%) EscapeArea()~ SOLVED_JOURNAL @20 EXIT
 END
 
-END // APPEND Aldeth
+END // APPEND ALDETH
