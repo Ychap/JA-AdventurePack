2109a764ceb7e8424f04df83de79d4267ae92e8a
Ychap <asdfsa85@web.de>
2024-03-02 22:04:27 +0100

    Separate Aldeth's party

---
+++ JA#BGT_AdvPack/Sashenstar_Haendlerliga/ja#aldeth_party.d
@@ -0,0 +1,111 @@
+// GLOBAL "TalkedToZorlIrenl" used in BGEE
+ADD_TRANS_ACTION IRLENT
+BEGIN 0 END
+BEGIN END
+~SetGlobal("TalkedToZorlIrenl","GLOBAL",1)~
+
+ADD_TRANS_ACTION ZORL
+BEGIN 0 END
+BEGIN END
+~SetGlobal("TalkedToZorlIrenl","GLOBAL",1)~
+
+ADD_TRANS_TRIGGER ALDETH 14
+~Global("TalkedToZorlIrenl","GLOBAL",1)~ DO 1
+
+
+// ADD_STATE_TRIGGER ALDETH 14
+// ~!Global("JA#CUTSCENE_ACTIVE","%SWBaldursGate_MerchantLeague_L3%",1) !Global("ZorlDopple","GLOBAL",1)~
+// ADD_STATE_TRIGGER ALDETH 21
+// ~!Global("JA#CUTSCENE_ACTIVE","%SWBaldursGate_MerchantLeague_L3%",1)~
+// ADD_STATE_TRIGGER ALDETH 24
+// ~!Global("JA#CUTSCENE_ACTIVE","%SWBaldursGate_MerchantLeague_L3%",1)~
+// ADD_STATE_TRIGGER ALDETH 26
+// ~!Global("JA#CUTSCENE_ACTIVE","%SWBaldursGate_MerchantLeague_L3%",1)~
+// ADD_STATE_TRIGGER ALDETH 27
+// ~!Global("JA#CUTSCENE_ACTIVE","%SWBaldursGate_MerchantLeague_L3%",1)~
+
+
+ALTER_TRANS IRLENT
+BEGIN 3 END
+BEGIN 0 END
+BEGIN
+  "EPILOGUE" ~EXTERN ALDETH JA#ALDETH_02~
+END
+
+ALTER_TRANS ZORL
+BEGIN 6 END
+BEGIN 0 END
+BEGIN
+  "EPILOGUE" ~EXTERN IRLENT 4~
+END
+
+ALTER_TRANS IRLENT
+BEGIN 4 END
+BEGIN 0 END
+BEGIN
+  "EPILOGUE" ~EXTERN ZORL 7~
+END
+
+ALTER_TRANS ZORL
+BEGIN 7 END
+BEGIN 0 END
+BEGIN
+  "ACTION" ~SetGlobal("ZorlDopple","GLOBAL",1)~
+  "EPILOGUE" ~GOTO 11~
+END
+
+
+EXTEND_BOTTOM ALDETH 14
+IF ~PartyHasItem("SCRL2P")~ THEN REPLY @1 DO ~SetGlobal("JA#ALDETH_KNOWDOPP","GLOBAL",1)~ UNSOLVED_JOURNAL @1025 GOTO JA#ALDETH_30
+IF ~!PartyHasItem("SCRL2P") GlobalGT("JA#DOPPELGANGER_SPAWN","GLOBAL",0) Global("JA#SENDZORL","GLOBAL",2)~ THEN REPLY @2 GOTO JA#ALDETH_31
+END
+
+
+APPEND ALDETH
+
+IF WEIGHT #-1 ~Global("JA#CUTSCENE_ACTIVE","%SWBaldursGate_MerchantLeague_L3%",1) Global("JA#ALDETH_KNOWDOPP","GLOBAL",0)~ THEN JA#ALDETH_01
+SAY @24
+IF ~~ THEN EXTERN IRLENT 3
+END
+
+IF ~~ THEN BEGIN JA#ALDETH_02
+SAY @27
+IF ~~ THEN EXTERN ZORL 6
+END
+
+IF WEIGHT #-1 ~Global("JA#CUTSCENE_ACTIVE","%SWBaldursGate_MerchantLeague_L3%",1) Global("JA#ALDETH_KNOWDOPP","GLOBAL",1)~ THEN JA#ALDETH_Y1
+SAY @25 = @26
+IF ~~ THEN EXTERN ZORL JA#ZORL_100
+END
+
+IF ~~ THEN BEGIN JA#ALDETH_30
+SAY @31 = @32
+IF ~~ THEN DO ~SetGlobal("JA#CUIRL","GLOBAL",1) SetGlobal("JA#ALD_MOVE1","%SWBaldursGate_MerchantLeague_L2%",1) EscapeArea()~ EXIT
+END
+
+IF ~~ THEN BEGIN JA#ALDETH_31
+SAY @33
+IF ~~ THEN DO ~SetGlobal("JA#CUIRL","GLOBAL",1) SetGlobal("JA#ALD_MOVE1","%SWBaldursGate_MerchantLeague_L2%",1) EscapeArea()~ EXIT
+END
+
+/* new dialogue state - if PC attacked Zorl or Irlentree and forced them to change without the invitation to the 2nd floor */
+
+IF WEIGHT #-1
+~Global("ZorlDopple","GLOBAL",1) GlobalLT("AldethDopple","GLOBAL",6)
+Global("Chapter","GLOBAL",%tutu_chapter_5%)
+Global("HelpAldeth","GLOBAL",1)~ THEN fight_started
+SAY @69
+IF ~~ THEN EXIT
+END
+
+END // APPEND ALDETH
+
+
+APPEND ZORL
+
+IF ~~ THEN BEGIN JA#ZORL_100
+SAY @42
+IF ~~ THEN EXTERN IRLENT 4
+END
+
+END // APPEND ZORL
