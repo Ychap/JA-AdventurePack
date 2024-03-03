9b8b037abcbbc82484ba0ab4072645c7436cad6d
Ychap <asdfsa85@web.de>
2024-03-03 18:16:58 +0100

    Use GET_DLG_JOURNAL_STRREF in Seniyad

--- JA#BGT_AdvPack/Seniyad/ja#aldeth_reward.d
+++ JA#BGT_AdvPack/Seniyad/ja#aldeth_reward.d
@@ -5,7 +5,7 @@ REPLACE_ACTION_TEXT ALDETH
 
 APPEND ALDETH
 
-IF ~GlobalLT("Chapter","GLOBAL",%tutu_chapter_5%) Global("SeniyadXP","GLOBAL",1)~ THEN BEGIN JA#ALDETH_X0
+IF WEIGHT #3 ~GlobalLT("Chapter","GLOBAL",%tutu_chapter_5%) Global("SeniyadXP","GLOBAL",1)~ THEN BEGIN JA#ALDETH_X0
 SAY @18 = @19
 IF ~~ THEN DO ~SetGlobal("AldethMove","GLOBAL",1) GiveItem("JA#ALDET",LastTalkedToBy) Shout(5) AddexperienceParty(600) EraseJournalEntry(%state04_journal%) EscapeArea()~ SOLVED_JOURNAL @20 EXIT
 END
