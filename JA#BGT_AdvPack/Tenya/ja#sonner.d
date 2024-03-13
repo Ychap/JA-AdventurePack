15c068632bbc6aa6881ce8c1ae70a9cfacfced11
Ychap <asdfsa85@web.de>
2024-03-13 22:18:16 +0100

    Platform specific dialog changes in  ja#sonner.d

--- JA#BGT_AdvPack/Tenya/ja#sonner.d
+++ JA#BGT_AdvPack/Tenya/ja#sonner.d
@@ -1,7 +1,16 @@
-ADD_TRANS_ACTION SONNER
-BEGIN 13 17 END
+// BGT
+REPLACE_TRANS_ACTION SONNER
+BEGIN 8 END
 BEGIN END
-~%state18_response0_action_end%~
+~ActionOverride("Telman",EscapeAreaDestroy(90))[%WNL%%MNL%%LNL%%TAB% ]*ActionOverride("Jebadoh",EscapeAreaDestroy(90))[%WNL%%MNL%%LNL%%TAB% ]*EscapeAreaDestroy(90)~
+~NoAction()~
+
+// state 10 only BGEE/EET
+ADD_TRANS_ACTION SONNER
+BEGIN 10 13 17 END
+BEGIN END
+~%fishermenEscapeArea%~
+UNLESS ~EscapeArea~
 
 
 EXTEND_BOTTOM SONNER 14
