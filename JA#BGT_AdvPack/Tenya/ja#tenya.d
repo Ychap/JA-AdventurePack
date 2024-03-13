c045f2ee62abe68099603f11e4f17b2cd801a72e
Ychap <asdfsa85@web.de>
2024-03-13 23:54:22 +0100

    Remove obsolete code in ja#tenya.d

--- JA#BGT_AdvPack/Tenya/ja#tenya.d
+++ JA#BGT_AdvPack/Tenya/ja#tenya.d
@@ -1,34 +1,14 @@
 
 /* This is a fix */
-ALTER_TRANS TENYA // file name
-BEGIN 5 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
+ALTER_TRANS TENYA
+BEGIN 5 END
+BEGIN 0 END
+BEGIN
   "TRIGGER" ~Global("HostileFishermen","GLOBAL",0)~
 END
 
-/* this doesn't change anything in my install */
-ALTER_TRANS TENYA // file name
-BEGIN 8 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "TRIGGER" ~Dead("Jebadoh")
-Dead("Telman")
-Dead("Sonner")~
-END
 
-/* this doesn't change anything in my install */
-ALTER_TRANS TENYA // file name
-BEGIN 8 END // state number (can be more than one)
-BEGIN 1 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "TRIGGER" ~PartyHasItem("MISC53")~
-END
-
-/* this won't show because Garrick (bg1npc + GToaT and Finch have I_C_T and are likely to be installed before JAP */
-EXTEND_BOTTOM TENYA 8
-IF ~PartyHasItem("MISC53")
-Dead("Jebadoh")
-Dead("Telman")
-Dead("Sonner")~ THEN REPLY @25 GOTO 10
+/* this won't show if garrick-tt is installed before */
+EXTEND_TOP TENYA 8 #1
+IF ~PartyHasItem("MISC53") Dead("Jebadoh") Dead("Telman") Dead("Sonner")~ THEN REPLY @25 GOTO 10
 END
