6a1272c9ab4eb6a50072f3c545c028514fecfb4d
Ychap <asdfsa85@web.de>
2024-03-16 13:14:48 +0100

    Remove destructive "fix" for TENYA.DLG

--- JA#BGT_AdvPack/Tenya/ja#tenya.d
+++ JA#BGT_AdvPack/Tenya/ja#tenya.d
@@ -1,13 +1,3 @@
-
-/* This is a fix */
-ALTER_TRANS TENYA
-BEGIN 5 END
-BEGIN 0 END
-BEGIN
-  "TRIGGER" ~Global("HostileFishermen","GLOBAL",0)~
-END
-
-
 /* this won't show if garrick-tt would be installed before */
 EXTEND_TOP TENYA 8 #1
 IF ~PartyHasItem("MISC53") Dead("Jebadoh") Dead("Telman") Dead("Sonner")~ THEN REPLY @0 GOTO 10
