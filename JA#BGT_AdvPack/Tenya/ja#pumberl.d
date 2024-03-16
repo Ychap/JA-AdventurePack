5e3e9ad01bc7459f47dc60fdcb5eeb68ff401ab8
Ychap <asdfsa85@web.de>
2024-03-16 12:19:12 +0100

    Shorter ja#pumberl.d

--- JA#BGT_AdvPack/Tenya/ja#pumberl.d
+++ JA#BGT_AdvPack/Tenya/ja#pumberl.d
@@ -1,32 +1,8 @@
-ALTER_TRANS PUMBERL // file name
-BEGIN 2 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "ACTION" ~NoAction()~
-END
+SET_WEIGHT PUMBERL 11 #-1 // IF ~StateCheck(Myself,STATE_CHARMED)~
 
-ALTER_TRANS PUMBERL // file name
-BEGIN 4 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "ACTION" ~NoAction()~
-END
-
-ALTER_TRANS PUMBERL // file name
-BEGIN 6 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "ACTION" ~SetGlobal("JALAPPEAR","GLOBAL",1)
-CreateCreature("JALANT",[459.531],3)
-ActionOverride("Jalantha",Dialogue([PC]))~
-END
-
-ALTER_TRANS PUMBERL // file name
-BEGIN 7 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "ACTION" ~SetGlobal("TENAPPEAR","GLOBAL",1)
-CreateCreature("TENYA2",[536.648],3) // Tenya
-ActionOverride("Tenya2",SetNumTimesTalkedTo(1))
-ActionOverride("Tenya2",Dialogue([PC]))~
+ALTER_TRANS PUMBERL
+BEGIN 2 4 END
+BEGIN 0 END
+BEGIN
+  "ACTION" ~NoAction()~ // Replace ~Wait(10) Enemy()~
 END
