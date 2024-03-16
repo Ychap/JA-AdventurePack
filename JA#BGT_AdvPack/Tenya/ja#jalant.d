79317822340c6b674a68c93ec2f663e2c9092c12
Ychap <asdfsa85@web.de>
2024-03-16 12:17:55 +0100

    Move PUMBERL.DLG modifications into separate file

--- JA#BGT_AdvPack/Tenya/ja#jalant.d
+++ JA#BGT_AdvPack/Tenya/ja#jalant.d
@@ -1,37 +1,3 @@
-ALTER_TRANS PUMBERL // file name
-BEGIN 2 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "ACTION" ~NoAction()~
-END
-
-ALTER_TRANS PUMBERL // file name
-BEGIN 4 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "ACTION" ~NoAction()~
-END
-
-ALTER_TRANS PUMBERL // file name
-BEGIN 6 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "ACTION" ~SetGlobal("JALAPPEAR","GLOBAL",1)
-CreateCreature("JALANT",[459.531],3)
-ActionOverride("Jalantha",Dialogue([PC]))~
-END
-
-ALTER_TRANS PUMBERL // file name
-BEGIN 7 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "ACTION" ~SetGlobal("TENAPPEAR","GLOBAL",1)
-CreateCreature("TENYA2",[536.648],3) // Tenya
-ActionOverride("Tenya2",SetNumTimesTalkedTo(1))
-ActionOverride("Tenya2",Dialogue([PC]))~
-END
-
-
 ALTER_TRANS JALANT // file name
 BEGIN 11 END // state number (can be more than one)
 BEGIN 0 END // transition number (can be more than one)
