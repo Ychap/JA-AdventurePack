975aa9db7a2142eb540815471e697988bf979508
Ychap <asdfsa85@web.de>
2024-03-16 12:25:40 +0100

    Shorter ja#jalant.d

--- JA#BGT_AdvPack/Tenya/ja#jalant.d
+++ JA#BGT_AdvPack/Tenya/ja#jalant.d
@@ -1,15 +1,3 @@
-ALTER_TRANS JALANT // file name
-BEGIN 11 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "ACTION" ~SetGlobal("JA#UMBERL_HOST","%BaldursGateDocks_WaterQueensHouse%",1)
-Enemy()~
-END
-
-ALTER_TRANS JALANT // file name
-BEGIN 14 END // state number (can be more than one)
-BEGIN 0 END // transition number (can be more than one)
-BEGIN // list of changes, see below for flags
-  "ACTION" ~SetGlobal("JA#UMBERL_HOST","%BaldursGateDocks_WaterQueensHouse%",1)
-Enemy()~
-END
+REPLACE_ACTION_TEXT JALANT
+~Enemy()~
+~SetGlobal("JA#UMBERL_HOST","%BaldursGateDocks_WaterQueensHouse%",1) \0~
