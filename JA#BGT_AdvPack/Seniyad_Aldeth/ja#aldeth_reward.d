809108d67cc2a8cedde1dfb97d1ce3ecb74e6eba
Ychap <asdfsa85@web.de>
2024-03-17 21:38:55 +0100

    Add trigger "druid_escape" in Seniyad & Aldeth

--- JA#BGT_AdvPack/Seniyad_Aldeth/ja#aldeth_reward.d
+++ JA#BGT_AdvPack/Seniyad_Aldeth/ja#aldeth_reward.d
@@ -6,8 +6,8 @@ REPLACE_ACTION_TEXT ALDETH
 APPEND ALDETH
 
 IF WEIGHT #%aldeth_state07_weight% ~Global("SeniyadXP","GLOBAL",1) Global("HelpAldeth","GLOBAL",1) GlobalLT("Chapter","GLOBAL",%tutu_chapter_5%)~ THEN BEGIN JA#ALDETH_X0
-SAY @18 = @19
-IF ~~ THEN DO ~SetGlobal("AldethMove","GLOBAL",1) GiveItem("JA#ALDET",LastTalkedToBy) Shout(5) AddexperienceParty(600) EraseJournalEntry(%aldeth_state04_journal%) EscapeArea()~ SOLVED_JOURNAL @20 EXIT
+  SAY @18 = @19
+  IF ~~ THEN DO ~SetGlobal("AldethMove","GLOBAL",1) GiveItem("JA#ALDET",LastTalkedToBy) Shout(5) AddexperienceParty(600) EraseJournalEntry(%aldeth_state04_journal%) EscapeArea()~ SOLVED_JOURNAL @20 EXIT
 END
 
 END // APPEND ALDETH
