eb6daea4ed452c76f2115734a40aebea515a8729
Ychap <asdfsa85@web.de>
2024-03-23 20:52:51 +0100

    Correct order in Improved dialogues

--- JA#BGT_AdvPack/Improved_dialogues/JA#Jondal.d
+++ JA#BGT_AdvPack/Improved_dialogues/JA#Jondal.d
@@ -1,20 +1,8 @@
-//de-bugged Jondalar Fix
-
-
-ADD_STATE_TRIGGER JONDAL 0 ~Global("JonHit","GLOBAL",0)~
-ADD_STATE_TRIGGER JONDAL 3 ~NumTimesTalkedToGT(0)~
-
 APPEND JONDAL
 
-IF ~NumTimesTalkedTo(0)
-Global("JonHit","GLOBAL",1)~ THEN BEGIN JA#JONDAL_0
-SAY @10
-IF ~~ THEN DO ~SetGlobal("JonHit","GLOBAL",2)
-ChangeEnemyAlly(Myself,NEUTRAL)
-ChangeEnemyAlly("Erik",NEUTRAL)
-ClearAllActions()
-ActionOverride("Erik",EscapeAreaDestroy(90))
-EscapeAreaDestroy(90)~ EXIT
+IF WEIGHT #-1 ~NumTimesTalkedTo(0) Global("JonHit","GLOBAL",1)~ THEN BEGIN JA#JONDAL_0
+  SAY @10
+  COPY_TRANS JONDAL 3
 END
 
-END
+END // JONDAL
