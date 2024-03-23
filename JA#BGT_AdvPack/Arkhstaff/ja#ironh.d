4c2721bbce336dc2d4ad32b8a1bcbcec9d148e60
Ychap <asdfsa85@web.de>
2024-03-23 13:34:15 +0100

    IF ~~ THEN BEGIN => IF ~~ THEN

--- JA#BGT_AdvPack/Arkhstaff/ja#ironh.d
+++ JA#BGT_AdvPack/Arkhstaff/ja#ironh.d
@@ -1,26 +1,26 @@
 BEGIN ~JA#IRONH~
 
-IF ~StateCheck(Myself,STATE_CHARMED) Global("JA#THRONE_DOOR","%BaldursGateDocks%",0)~ THEN BEGIN 0
+IF ~StateCheck(Myself,STATE_CHARMED) Global("JA#THRONE_DOOR","%BaldursGateDocks%",0)~ THEN 0
   SAY @0
   IF ~~ THEN DO ~SetGlobal("JA#THRONE_DOOR","%BaldursGateDocks%",1)~ EXIT
 END
 
-IF ~StateCheck(Myself,STATE_CHARMED)~ THEN BEGIN 1
+IF ~StateCheck(Myself,STATE_CHARMED)~ THEN 1
   SAY @0
   IF ~~ THEN EXIT
 END
 
-IF ~GlobalGT("SearchIronThrone","GLOBAL",0)~ THEN BEGIN 2
+IF ~GlobalGT("SearchIronThrone","GLOBAL",0)~ THEN 2
   SAY @1
   IF ~~ THEN DO ~Enemy()~ EXIT
 END
 
-IF ~Global("JA#THRONE_DOOR","%BaldursGateDocks%",0)~ THEN BEGIN 3
+IF ~Global("JA#THRONE_DOOR","%BaldursGateDocks%",0)~ THEN 3
   SAY @2
   IF ~~ THEN EXIT
 END
 
-IF ~Global("JA#THRONE_DOOR","%BaldursGateDocks%",1)~ THEN BEGIN 4
+IF ~Global("JA#THRONE_DOOR","%BaldursGateDocks%",1)~ THEN 4
   SAY @3
   IF ~~ THEN EXIT
 END
