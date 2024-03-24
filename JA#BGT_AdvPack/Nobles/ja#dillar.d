9add5c136c47345e916df28932f2249148fb569d
Ychap <asdfsa85@web.de>
2024-03-24 12:26:25 +0100

    Reorder Nobles

--- JA#BGT_AdvPack/Nobles/ja#dillar.d
+++ JA#BGT_AdvPack/Nobles/ja#dillar.d
@@ -1,3 +1,1 @@
-//Dillar
-
 ADD_STATE_TRIGGER DILLAR 6 ~True()~
--- JA#BGT_AdvPack/Nobles/ja#yago.d
+++ JA#BGT_AdvPack/Nobles/ja#yago.d
@@ -1,5 +1,3 @@
-//Yago
-
 ADD_TRANS_TRIGGER YAGO 0
 ~Global("BrielbaraMove","GLOBAL",1)~ DO 2
 UNLESS ~Global("BrielbaraMove","GLOBAL"~
