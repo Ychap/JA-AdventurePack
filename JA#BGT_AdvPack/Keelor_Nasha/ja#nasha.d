deb1caccdcc11c798ced9eb7d70cca485ff6810b
Ychap <asdfsa85@web.de>
2024-03-27 21:43:47 +0100

    Remove bugged script for Nasha

--- JA#BGT_AdvPack/Keelor_Nasha/ja#nasha.d
+++ JA#BGT_AdvPack/Keelor_Nasha/ja#nasha.d
@@ -7,27 +7,15 @@ END
 
 IF ~Global("JA#NASHA","GLOBAL",1)~ THEN 1
   SAY @1
-  IF ~~ THEN DO ~SetGlobal("JA#NASHA","GLOBAL",2) SetGlobal("JA#NASHA_HEAL","LOCALS",1)~ EXIT
+  IF ~~ THEN DO ~SetGlobal("JA#NASHA","GLOBAL",2)~ EXIT
 END
 
-IF ~Global("JA#NASHA","GLOBAL",2)
-OR(6)
-HPPercentLT(Player1,100)
-HPPercentLT(Player2,100)
-HPPercentLT(Player3,100)
-HPPercentLT(Player4,100)
-HPPercentLT(Player5,100)
-HPPercentLT(Player6,100)~ THEN 2
+IF ~Global("JA#NASHA","GLOBAL",2) HPPercentLT(LastTalkedToBy,100)~ THEN 2
   SAY @2
-  IF ~~ THEN DO ~SetGlobal("JA#NASHA_HEAL","LOCALS",1)~ EXIT
+  IF ~~ THEN DO ~ForceSpell(LastTalkedToBy,CLERIC_HEAL)~ EXIT
 END
 
-IF ~HPPercent(Player1,100)
-HPPercent(Player2,100)
-HPPercent(Player3,100)
-HPPercent(Player4,100)
-HPPercent(Player5,100)
-HPPercent(Player6,100)~ THEN 3
+IF ~HPPercent(LastTalkedToBy,100)~ THEN 3
   SAY @3
   IF ~~ THEN EXIT
 END
