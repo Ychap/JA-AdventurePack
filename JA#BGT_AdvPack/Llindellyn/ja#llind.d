727ddf7f604e820905a802dcf4023fe969c03d20
Ychap <asdfsa85@web.de>
2024-03-27 18:08:01 +0100

    Additional Dialog for Llindellyn

--- JA#BGT_AdvPack/Llindellyn/ja#llind.d
+++ JA#BGT_AdvPack/Llindellyn/ja#llind.d
@@ -8,7 +8,7 @@ END
 
 IF ~~ THEN 1
   SAY @3
-  IF ~~ THEN REPLY @4 GOTO 2
+  IF ~~ THEN GOTO 2
 END
 
 IF ~~ THEN 2
@@ -16,24 +16,24 @@ IF ~~ THEN 2
   IF ~~ THEN DO ~SetGlobal("JA#LLIND_JOB","LOCALS",1)~ UNSOLVED_JOURNAL @6 EXIT
 END
 
-IF ~Global("JA#LLIND_JOB","LOCALS",1)!PartyHasItem("JA#LLIND")~ THEN 3
+IF ~Global("JA#LLIND_JOB","LOCALS",1) !PartyHasItem("JA#LLIND")~ THEN 3
   SAY @7
-  IF ~~ THEN EXIT
+  IF ~~ THEN GOTO 9
 END
 
-IF ~ReactionLT(LastTalkedToBy,NEUTRAL_LOWER)PartyHasItem("JA#LLIND")~ THEN 4
+IF ~PartyHasItem("JA#LLIND") ReactionLT(LastTalkedToBy,NEUTRAL_LOWER)~ THEN 4
   SAY @8
-  IF ~~ THEN DO ~TakePartyItem("JA#LLIND")GiveItem("AROW11",LastTalkedToBy(Myself))SetGlobal("JA#LLIND_JOB","LOCALS",2)AddexperienceParty(150)EraseJournalEntry(@6)~ SOLVED_JOURNAL @9 EXIT
+  IF ~~ THEN DO ~TakePartyItem("JA#LLIND") GiveItem("AROW11",LastTalkedToBy(Myself)) SetGlobal("JA#LLIND_JOB","LOCALS",2) AddexperienceParty(150) EraseJournalEntry(@6)~ SOLVED_JOURNAL @9 EXIT
 END
 
-IF ~ReactionGT(LastTalkedToBy,HOSTILE_UPPER)ReactionLT(LastTalkedToBy,FRIENDLY_LOWER)PartyHasItem("JA#LLIND")~ THEN 5
+IF ~PartyHasItem("JA#LLIND") ReactionGT(LastTalkedToBy,HOSTILE_UPPER) ReactionLT(LastTalkedToBy,FRIENDLY_LOWER)~ THEN 5
   SAY @10
-  IF ~~ THEN DO ~TakePartyItem("JA#LLIND")GiveItem("AROW10",LastTalkedToBy(Myself))SetGlobal("JA#LLIND_JOB","LOCALS",2)AddexperienceParty(150)EraseJournalEntry(@6)~ SOLVED_JOURNAL @9 EXIT
+  IF ~~ THEN DO ~TakePartyItem("JA#LLIND") GiveItem("AROW10",LastTalkedToBy(Myself)) SetGlobal("JA#LLIND_JOB","LOCALS",2) AddexperienceParty(150) EraseJournalEntry(@6)~ SOLVED_JOURNAL @9 EXIT
 END
 
-IF ~PartyHasItem("JA#LLIND")ReactionGT(LastTalkedToBy,NEUTRAL_UPPER)~ THEN 6
+IF ~PartyHasItem("JA#LLIND") ReactionGT(LastTalkedToBy,NEUTRAL_UPPER)~ THEN 6
   SAY @11
-  IF ~~ THEN DO ~TakePartyItem("JA#LLIND")GiveItem("AROW06",LastTalkedToBy(Myself))SetGlobal("JA#LLIND_JOB","LOCALS",2)AddexperienceParty(150)EraseJournalEntry(@6)~ SOLVED_JOURNAL @9 EXIT
+  IF ~~ THEN DO ~TakePartyItem("JA#LLIND") GiveItem("AROW06",LastTalkedToBy(Myself)) SetGlobal("JA#LLIND_JOB","LOCALS",2) AddexperienceParty(150) EraseJournalEntry(@6)~ SOLVED_JOURNAL @9 EXIT
 END
 
 IF ~~ THEN 7
@@ -45,3 +45,10 @@ IF ~Global("JA#LLIND_JOB","LOCALS",2)~ THEN 8
   SAY @13
   IF ~~ THEN EXIT
 END
+
+IF ~~ THEN 9
+  SAY @4
+  IF ~~ THEN EXIT
+END
+
+// END JA#LLIND
