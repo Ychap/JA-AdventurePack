c76897dd4e0b10fa96776467d6f99e3db0c384af
Ychap <asdfsa85@web.de>
2024-03-29 11:20:30 +0100

    Add Galtoth in BlushingMermaid

--- JA#BGT_AdvPack/Galtoth/ja#galto.d
+++ JA#BGT_AdvPack/Galtoth/ja#galto.d
@@ -6,8 +6,10 @@ IF ~NumTimesTalkedTo(0)~ THEN 0
   IF ~~ THEN REPLY @2 EXIT
 END
 
-IF ~NumTimesTalkedToGT(0)~ THEN 1
+IF ~True()~ THEN 1
   SAY @3
   IF ~~ THEN REPLY @4 DO ~StartStore("ja#galto",LastTalkedToBy(Myself))~ EXIT
   IF ~~ THEN REPLY @2 EXIT
 END
+
+// END JA#GALTO
