4894335be5495eb3980480d352bb26bbd4c7d19a
Ychap <asdfsa85@web.de>
2024-04-10 22:33:49 +0200

    Reorder Marl component

--- JA#BGT_AdvPack/Marl/ja#dunkin.d
+++ JA#BGT_AdvPack/Marl/ja#dunkin.d
@@ -1,5 +1,6 @@
 REPLACE_STATE_TRIGGER DUNKIN 5 ~Global("JA#DUNKIN","%Beregost_FeldepostsInn_L1%",1)~
 
+
 ADD_TRANS_ACTION DUNKIN
 BEGIN 5 END
 BEGIN END
@@ -9,4 +10,4 @@ BEGIN END
 ADD_TRANS_ACTION DUNKIN
 BEGIN 7 END
 BEGIN END
-~SetGlobal("JA#MARLDLG","LOCALS",1) ClearAllActions() ChangeEnemyAlly("Marl",NEUTRAL) ActionOverride("Marl",MoveToPoint([701.773]))~
+~ChangeEnemyAlly("Marl",NEUTRAL) ClearAllActions()~
--- JA#BGT_AdvPack/Marl/ja#keldda.d
+++ JA#BGT_AdvPack/Marl/ja#keldda.d
@@ -1,33 +1,11 @@
-ALTER_TRANS KELDDA
-BEGIN 4 END
-BEGIN 0 END
-BEGIN
-  "ACTION" ~SetGlobal("JA#MARL_GUARD","GLOBAL",3)
-ActionOverride("JA#GUAB1",EscapeArea())
-ActionOverride("JA#GUAB2",EscapeArea())
-ActionOverride("JA#GUAB3",EscapeArea())
-TakePartyGold(300)~
-END
-
-ALTER_TRANS KELDDA
-BEGIN 5 END
-BEGIN 0 END
-BEGIN
-  "ACTION" ~SetGlobal("JA#MARL_GUARD","GLOBAL",3)
-ActionOverride("JA#GUAB1",EscapeArea())
-ActionOverride("JA#GUAB2",EscapeArea())
-ActionOverride("JA#GUAB3",EscapeArea())
-TakePartyGold(200)~
-END
-
-
-ADD_STATE_TRIGGER KELDDA 1
-~GlobalLT("JA#MARL_GUARD","GLOBAL",1)~
+REPLACE_STATE_TRIGGER KELDDA 3 ~%state01_trigger%~
 
 SET_WEIGHT KELDDA 3 #%state01_weight%
 
-REPLACE_STATE_TRIGGER KELDDA 3
-~%state01_trigger%~
+
+ADD_STATE_TRIGGER KELDDA 1
+~Global("JA#MARL_GUARD","GLOBAL",0)~
+
 
 ALTER_TRANS KELDDA
 BEGIN 3 END
@@ -35,4 +13,20 @@ BEGIN END
 BEGIN
   "ACTION" ~SetGlobal("B!GavinBassilusQuest","GLOBAL",5) %state01_response0_action%~
   "SOLVED_JOURNAL" ~@0~
+END
+
+
+ALTER_TRANS KELDDA
+BEGIN 4 END
+BEGIN 0 END
+BEGIN
+  "ACTION" ~SetGlobal("JA#MARL_GUARD","GLOBAL",3) TakePartyGold(300) ActionOverride("JA#GUAB1",EscapeArea()) ActionOverride("JA#GUAB2",EscapeArea()) ActionOverride("JA#GUAB3",EscapeArea())~
+END
+
+
+ALTER_TRANS KELDDA
+BEGIN 5 END
+BEGIN 0 END
+BEGIN
+  "ACTION" ~SetGlobal("JA#MARL_GUARD","GLOBAL",3) TakePartyGold(200) ActionOverride("JA#GUAB1",EscapeArea()) ActionOverride("JA#GUAB2",EscapeArea()) ActionOverride("JA#GUAB3",EscapeArea())~
 END
