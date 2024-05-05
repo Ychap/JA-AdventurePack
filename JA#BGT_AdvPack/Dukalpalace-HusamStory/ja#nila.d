6b0df84e7f758d9bd0410b7403746439ecdfc594
Ychap <asdfsa85@web.de>
2024-05-05 12:45:16 +0200

    Order ja#nila.d

--- JA#BGT_AdvPack/Dukalpalace-HusamStory/ja#nila.d
+++ JA#BGT_AdvPack/Dukalpalace-HusamStory/ja#nila.d
@@ -1,20 +1,12 @@
-ADD_STATE_TRIGGER NILA 0
-~Global("JA#NILA","%NBaldursGate_DucalPalace_L2%",0) Global("JA#CONTAINER2","%NBaldursGate_DucalPalace_L2%",0)~
-ADD_STATE_TRIGGER NILA 1
-~Global("JA#NILA","%NBaldursGate_DucalPalace_L2%",0) Global("JA#CONTAINER2","%NBaldursGate_DucalPalace_L2%",0)~
+ADD_STATE_TRIGGER NILA 0 ~Global("JA#NILA","%NBaldursGate_DucalPalace_L2%",0) Global("JA#CONTAINER2","%NBaldursGate_DucalPalace_L2%",0)~
+ADD_STATE_TRIGGER NILA 1 ~Global("JA#NILA","%NBaldursGate_DucalPalace_L2%",0) Global("JA#CONTAINER2","%NBaldursGate_DucalPalace_L2%",0)~
 
-
-REPLACE_STATE_TRIGGER NILA 7
-~Global("JA#NILA","%NBaldursGate_DucalPalace_L2%",3) Global("JA#CONTAINER2","%NBaldursGate_DucalPalace_L2%",2)~
-
-REPLACE_STATE_TRIGGER NILA 8
-~True()~
-
+REPLACE_STATE_TRIGGER NILA 7 ~Global("JA#NILA","%NBaldursGate_DucalPalace_L2%",3) Global("JA#CONTAINER2","%NBaldursGate_DucalPalace_L2%",2)~
+REPLACE_STATE_TRIGGER NILA 8 ~True()~
+REPLACE_STATE_TRIGGER NILA 2 ~Global("JA#CONTAINER2","%NBaldursGate_DucalPalace_L2%",1)~
 
 SET_WEIGHT NILA 2 #-1
 
-REPLACE_STATE_TRIGGER NILA 2
-~Global("JA#CONTAINER2","%NBaldursGate_DucalPalace_L2%",1)~
 
 ADD_TRANS_ACTION NILA
 BEGIN 3 END
@@ -25,13 +17,7 @@ BEGIN END
 ADD_TRANS_TRIGGER NILA 0 ~False()~
 ADD_TRANS_TRIGGER NILA 1 ~False()~
 
-EXTEND_BOTTOM NILA 0
-  IF ~~ THEN REPLY @8 DO ~SetGlobal("JA#NILA_WHO","LOCALS",1)~ GOTO JA#NILA_3
-  IF ~~ THEN REPLY @0 DO ~SetGlobal("JA#SARE","LOCALS",1)~ GOTO JA#NILA_4
-  IF ~~ THEN REPLY @1 GOTO JA#NILA_5
-END
-
-EXTEND_BOTTOM NILA 1
+EXTEND_BOTTOM NILA 0 1
   IF ~~ THEN REPLY @8 DO ~SetGlobal("JA#NILA_WHO","LOCALS",1)~ GOTO JA#NILA_3
   IF ~~ THEN REPLY @0 DO ~SetGlobal("JA#SARE","LOCALS",1)~ GOTO JA#NILA_4
   IF ~~ THEN REPLY @1 GOTO JA#NILA_5
