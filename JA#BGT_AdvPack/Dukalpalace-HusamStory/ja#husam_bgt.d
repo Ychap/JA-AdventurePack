77c2f925de6687279ee8bf443cfadfcfb95282ae
Ychap <asdfsa85@web.de>
2024-05-05 09:02:54 +0200

    Align *.d files in Dukal Palace and Husam Story

--- JA#BGT_AdvPack/Dukalpalace-HusamStory/ja#husam_bgt.d
+++ JA#BGT_AdvPack/Dukalpalace-HusamStory/ja#husam_bgt.d
@@ -2,17 +2,12 @@ ALTER_TRANS HUSAM2
 BEGIN 3 END
 BEGIN 1 END
 BEGIN
-  "ACTION" ~ClearAllActions()
-StartCutSceneMode()
-StartCutScene("JA#HUCU1")~
+  "ACTION" ~ClearAllActions() StartCutSceneMode() StartCutScene("JA#HUCU1")~
 END
 
 ALTER_TRANS HUSAM2
 BEGIN 12 END
 BEGIN 0 END
 BEGIN
-  "ACTION" ~ClearAllActions()
-MultiplayerSync()
-StartCutSceneMode()
-StartCutScene("JA#HUCU3")~
+  "ACTION" ~ClearAllActions() StartCutSceneMode() StartCutScene("JA#HUCU3")~
 END
--- JA#BGT_AdvPack/Dukalpalace-HusamStory/ja#nila.d
+++ JA#BGT_AdvPack/Dukalpalace-HusamStory/ja#nila.d
@@ -26,55 +26,55 @@ ADD_TRANS_TRIGGER NILA 0 ~False()~
 ADD_TRANS_TRIGGER NILA 1 ~False()~
 
 EXTEND_BOTTOM NILA 0
-IF ~~ THEN REPLY @8 DO ~SetGlobal("JA#NILA_WHO","LOCALS",1)~ GOTO JA#NILA_3
-IF ~~ THEN REPLY @0 DO ~SetGlobal("JA#SARE","LOCALS",1)~ GOTO JA#NILA_4
-IF ~~ THEN REPLY @1 GOTO JA#NILA_5
+  IF ~~ THEN REPLY @8 DO ~SetGlobal("JA#NILA_WHO","LOCALS",1)~ GOTO JA#NILA_3
+  IF ~~ THEN REPLY @0 DO ~SetGlobal("JA#SARE","LOCALS",1)~ GOTO JA#NILA_4
+  IF ~~ THEN REPLY @1 GOTO JA#NILA_5
 END
 
 EXTEND_BOTTOM NILA 1
-IF ~~ THEN REPLY @8 DO ~SetGlobal("JA#NILA_WHO","LOCALS",1)~ GOTO JA#NILA_3
-IF ~~ THEN REPLY @0 DO ~SetGlobal("JA#SARE","LOCALS",1)~ GOTO JA#NILA_4
-IF ~~ THEN REPLY @1 GOTO JA#NILA_5
+  IF ~~ THEN REPLY @8 DO ~SetGlobal("JA#NILA_WHO","LOCALS",1)~ GOTO JA#NILA_3
+  IF ~~ THEN REPLY @0 DO ~SetGlobal("JA#SARE","LOCALS",1)~ GOTO JA#NILA_4
+  IF ~~ THEN REPLY @1 GOTO JA#NILA_5
 END
 
 
 APPEND NILA
 
 IF WEIGHT #-1 ~Global("JA#NILA","%NBaldursGate_DucalPalace_L2%",1)~ THEN JA#NILA_1
-SAY @2
-IF ~~ THEN DO ~SetGlobal("JA#NILA","%NBaldursGate_DucalPalace_L2%",2)
-DialogInterrupt(FALSE)
-MoveToPoint([587.578])
-Face(6)
-Wait(2)
-PlaySound("AMB_D21")
-Unlock("Container 1")
-Wait(1)
-DialogInterrupt(TRUE)
-Dialogue([PC])~ EXIT
+  SAY @2
+  IF ~~ THEN DO ~SetGlobal("JA#NILA","%NBaldursGate_DucalPalace_L2%",2)
+                 DialogInterrupt(FALSE)
+                 MoveToPoint([587.578])
+                 Face(6)
+                 Wait(2)
+                 PlaySound("AMB_D21")
+                 Unlock("Container 1")
+                 Wait(1)
+                 DialogInterrupt(TRUE)
+                 Dialogue([PC])~ EXIT
 END
 
 IF WEIGHT #-1 ~Global("JA#NILA","%NBaldursGate_DucalPalace_L2%",2)~ THEN JA#NILA_2
-SAY @3
-IF ~~ THEN DO ~SetGlobal("JA#NILA","%NBaldursGate_DucalPalace_L2%",3) MoveToPoint([522.640])~ EXIT
+  SAY @3
+  IF ~~ THEN DO ~SetGlobal("JA#NILA","%NBaldursGate_DucalPalace_L2%",3) MoveToPoint([522.640])~ EXIT
 END
 
 
 IF ~~ THEN JA#NILA_3
-SAY @4
-IF ~Global("JA#SARE","LOCALS",0)~ THEN REPLY @0 DO ~SetGlobal("JA#SARE","LOCALS",1)~ GOTO JA#NILA_4
-IF ~~ THEN REPLY @1 GOTO JA#NILA_5
+  SAY @4
+  IF ~Global("JA#SARE","LOCALS",0)~ THEN REPLY @0 DO ~SetGlobal("JA#SARE","LOCALS",1)~ GOTO JA#NILA_4
+  IF ~~ THEN REPLY @1 GOTO JA#NILA_5
 END
 
 IF ~~ THEN JA#NILA_4
-SAY @5
-IF ~Global("JA#NILA_WHO","LOCALS",0)~ THEN REPLY @6 DO ~SetGlobal("JA#NILA_WHO","LOCALS",1)~ GOTO JA#NILA_3
-IF ~~ THEN REPLY @1 GOTO JA#NILA_5
+  SAY @5
+  IF ~Global("JA#NILA_WHO","LOCALS",0)~ THEN REPLY @6 DO ~SetGlobal("JA#NILA_WHO","LOCALS",1)~ GOTO JA#NILA_3
+  IF ~~ THEN REPLY @1 GOTO JA#NILA_5
 END
 
 IF ~~ THEN JA#NILA_5
-SAY @7
-COPY_TRANS NILA 0
+  SAY @7
+  COPY_TRANS NILA 0
 END
 
 END // APPEND NILA
