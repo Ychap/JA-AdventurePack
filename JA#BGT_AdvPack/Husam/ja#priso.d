a1d73a20fb42f2af13ea837e4263d52ea2e0935e
Ychap <asdfsa85@web.de>
2024-10-13 13:26:19 +0200

    Remove guards from DucalPalace cellar

--- JA#BGT_AdvPack/Husam/ja#priso.d
+++ JA#BGT_AdvPack/Husam/ja#priso.d
@@ -81,26 +81,3 @@ IF ~~ THEN JA#PRISO_13
 END
 
 // END JA#PRISO
-
-
-
-BEGIN ~JA#FLAM1~
-
-IF ~True()~ THEN JA#FLAM1_0
-  SAY @34
-  IF ~~ THEN REPLY @35 EXIT
-  IF ~~ THEN REPLY @36 GOTO JA#FLAM1_1
-  IF ~~ THEN REPLY @37 GOTO JA#FLAM1_2
-END
-
-IF ~~ THEN JA#FLAM1_1
-  SAY @38
-  IF ~~ THEN DO ~SetGlobal("JA#ENEMY","%NBaldursGate_DucalPalace_Cellar%",1) Enemy()~ EXIT
-END
-
-IF ~~ THEN JA#FLAM1_2
-  SAY @39
-  IF ~~ THEN DO ~SetGlobal("JA#ENEMY","%NBaldursGate_DucalPalace_Cellar%",1) Enemy()~ EXIT
-END
-
-// END JA#FLAM1
