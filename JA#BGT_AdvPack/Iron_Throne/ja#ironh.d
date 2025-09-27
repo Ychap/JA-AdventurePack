895959fa4d7372cfdb358abc47e9b4cc051746cb
Ychap <asdfsa85@web.de>
2025-09-27 17:28:52 +0200

    Fix behavior of Iron Throne guard

--- JA#BGT_AdvPack/Iron_Throne/ja#ironh.d
+++ JA#BGT_AdvPack/Iron_Throne/ja#ironh.d
@@ -1,27 +1,22 @@
 BEGIN ~JA#IRONH~
 
-IF ~StateCheck(Myself,STATE_CHARMED) Global("JA#THRONE_DOOR","%BaldursGateDocks%",0)~ THEN 0
+IF ~Dead("ZHALIMAR") Dead("ALAI") Dead("AASIM") Dead("DIYAB") Dead("GARDUSH") Dead("NAAMAN")~ THEN 0
   SAY @0
-  IF ~~ THEN DO ~SetGlobal("JA#THRONE_DOOR","%BaldursGateDocks%",1)~ EXIT
+  IF ~~ THEN DO ~EscapeArea()~ EXIT
 END
 
 IF ~StateCheck(Myself,STATE_CHARMED)~ THEN 1
   SAY @0
-  IF ~~ THEN EXIT
+  IF ~~ THEN DO ~Unlock("DOOR0611") OpenDoor("DOOR0611") EscapeArea()~ EXIT
 END
 
-IF ~GlobalGT("SearchIronThrone","GLOBAL",0)~ THEN 2
+IF ~Global("SearchIronThrone","GLOBAL",1)~ THEN 2
   SAY @1
   IF ~~ THEN DO ~Enemy()~ EXIT
 END
 
-IF ~Global("JA#THRONE_DOOR","%BaldursGateDocks%",0)~ THEN 3
+IF ~True()~ THEN 3
   SAY @2
-  IF ~~ THEN EXIT
-END
-
-IF ~Global("JA#THRONE_DOOR","%BaldursGateDocks%",1)~ THEN 4
-  SAY @3
   IF ~~ THEN EXIT
 END
 
